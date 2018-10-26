3d31248f0f17885eda10545d0657ba5989f4ee0f
Blakers1015 <42722793+Blakers1015@users.noreply.github.com>
2018-10-25 19:37:44 -0500

    Fixed main activity and layout

--- app/src/main/java/com/example/s523353/justkeepfishing/MainActivity.java
+++ app/src/main/java/com/example/s523353/justkeepfishing/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.s523353.justkeepfishing;
 
+import android.content.Intent;
+import android.graphics.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("test changes");
-        System.out.println("hi");
+
+    }
+    public void Gallery(View v)
+    {
+        Intent gallery = new Intent(this,Gallary.class);
+        startActivityForResult(gallery,1);
+    }
+    public void Camera(View V)
+    {
+        Intent camera = new Intent(this,Search.class);
+        startActivityForResult(camera,2);
     }
 }
