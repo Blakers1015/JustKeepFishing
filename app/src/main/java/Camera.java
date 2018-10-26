9383286f34368ef9f24e46fff1acbf23fb30e5d5
Blakers1015 <42722793+Blakers1015@users.noreply.github.com>
2018-10-26 16:32:03 -0500

    camera functionality

--- app/src/main/java/Camera.java
+++ app/src/main/java/Camera.java
@@ -1,2 +1,19 @@
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.s523353.justkeepfishing.R;
+
 public class Camera {
+//    Camera=(Button)findViewById(R.id.CameraID);
+//Camera.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            // TODO Auto-generated method stub
+//            Intent Intent3=new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+//            startActivity(Intent3);
+//        }
+//    });
+
 }
