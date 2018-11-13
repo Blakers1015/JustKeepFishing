54cb2db29b46d1bc68215bdfe7be72aada7fe43e
Blakers1015 <42722793+Blakers1015@users.noreply.github.com>
2018-11-13 09:41:40 -0600

    more GPS Tracking

--- app/src/main/java/com/example/s523353/justkeepfishing/MainActivity.java
+++ app/src/main/java/com/example/s523353/justkeepfishing/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +39,45 @@ public class MainActivity extends AppCompatActivity {
         Intent camera = new Intent(this,Search.class);
         startActivityForResult(camera,2);
     }
+    GPSTracker gps;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if the result is capturing Image
+        int CAMERA_CAPTURE_IMAGE_REQUEST_CODE;
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+            gps = new GPSTracker(AndroidGPSTrackingActivity.this);
 
+            if (resultCode == RESULT_OK)
+            {
+                previewCapturedImage();
+                if(gps.canGetLocation())
+                {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                } else {
+                    // Can't get location.
+                    // GPS or network is not enabled.
+                    // Ask user to enable GPS/network in settings.
+
+                }
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "Cancelled", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                // failed to capture image
+                Toast.makeText(getApplicationContext(),
+                        "Error!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+
+    }
 
 
 
